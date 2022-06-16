877d8d799817c90b8f2c6bc35e48e61a10489cbb
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-06-16 11:39:17 -0230

    background?
    trying to work out how to add background.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import KnittingTitle from './KnittingTitle'
 import NewOrView from './NewOrView'
+import wool from "./images/wool.jpg"
 
 const App = () => {
   return (
-    <>
+    <div styles={{backgroundImage:`url(${wool})`,
+    height: '100px',
+    width: '100px'}}>
       <KnittingTitle />
       <NewOrView Title="New Project" Button="New" />
       <NewOrView Title="View Projects" Button="View" />
-    </>
+    </div>
   )
 }
 
