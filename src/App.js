0124bef402ced7754457e46afb59b3995581bd28
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-06-22 18:43:34 -0230

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { NewOrView } from "./components/NewOrView";
 import wool from "./images/wool.jpg";
 
 const App = () => {
-  <div
-    style={{
+  <>
+    style=
+    {{
       backgroundImage: `url(${wool})`,
       height: "100vh",
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
       marginTop: "-21px",
     }}
-  >
     <KnittingTitle />
     <NewOrView Title="New Project" Button="New" />
     <NewOrView Title="View Projects" Button="View" />
-  </div>;
+  </>;
 };
 
 export default App;
