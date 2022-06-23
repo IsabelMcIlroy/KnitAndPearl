553b91309d1b3a67e161f64721cb7fee2100a4fb
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-06-23 11:11:35 -0230

    added logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { KnittingTitle } from "./components/KnittingTitle";
 import { NewOrView } from "./components/NewOrView";
 import wool from "./images/wool.jpg";
+import logo from "./images/logo.png";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
     >
       <KnittingTitle />
       <NewOrView btnText={"new"} />
+      <img src={logo} alt="ball of wool" />
       <NewOrView btnText={"view"} />
     </div>
   );
