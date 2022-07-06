4844d8c3c8065870132b6da8909fa96d4a66203d
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-07-06 13:47:00 -0230

    selections

--- src/components/NewProjectSizeAndColourSelectionModal.js
+++ src/components/NewProjectSizeAndColourSelectionModal.js
@@ -2,7 +2,9 @@ import { useForm } from "react-hook-form";
 import Modal from "@mui/material/Modal";
 import { ThemeProvider } from "@mui/material";
 import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
 import Button from "@mui/material/Button";
 import KnittingTheme from "../assets/Theme";
 import { modalTitle } from "../assets/Theme";
@@ -21,10 +23,48 @@ export const NewProjectSizeAndColourSelectionModal = ({
             New Knitting Project
           </Typography>
           <form onSubmit={handleSubmit()}>
-            <TextField
-              {...register("projectName")}
-              placeholder="Project Name"
-            />
+            <Box>
+              <TextField
+                {...register("projectName")}
+                placeholder="Project Name*"
+              />
+            </Box>
+            <Box>
+              <TextField
+                {...register("projectType")}
+                placeholder="Project Type"
+              />
+            </Box>
+            <Box>
+              <Select native label="Row">
+                <option value="">Row*</option>
+                <option value={2}>Two</option>
+                <option value={3}>Three</option>
+                <option value={4}>Four</option>
+                <option value={5}>Five</option>
+                <option value={6}>Six</option>
+                <option value={7}>Seven</option>
+                <option value={8}>Eight</option>
+                <option value={9}>Nine</option>
+                <option value={10}>Ten</option>
+                <option value={11}>Eleven</option>
+                <option value={12}>Twelve</option>
+              </Select>
+              <Select native label="Column">
+                <option value="">Column*</option>
+                <option value={2}>Two</option>
+                <option value={3}>Three</option>
+                <option value={4}>Four</option>
+                <option value={5}>Five</option>
+                <option value={6}>Six</option>
+                <option value={7}>Seven</option>
+                <option value={8}>Eight</option>
+                <option value={9}>Nine</option>
+                <option value={10}>Ten</option>
+                <option value={11}>Eleven</option>
+                <option value={12}>Twelve</option>
+              </Select>
+            </Box>
             <Button type="submit">Submit</Button>
           </form>
         </>
