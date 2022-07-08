c5d49cfe742fd00ce38f211a9a4687ece054814e
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-07-08 13:15:25 -0230

    NavBar

--- src/ViewProject.js
+++ src/ViewProject.js
@@ -1,42 +1,5 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
-import { AppBar } from "@mui/material";
-import Toolbar from "@mui/material/Toolbar";
-import Grid from "@mui/material/Grid";
-
-import wool from "./images/wool.jpg";
-
-import { WelcomePageOptionButtons } from "./components/WelcomePageOptionButtons";
-import { Logo } from "./components/Logo";
-import { NewProjectSizeAndColourSelectionModal } from "./components/NewProjectSizeAndColourSelectionModal";
+import { NavBar } from "./components/NavBar";
 
 export const ViewProject = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <>
-      <AppBar
-        postion="sticky"
-        sx={{ backgroundImage: `url(${wool})`, padding: "10px" }}
-      >
-        <Toolbar>
-          <Grid container direction="row" justifyContent="flex-end">
-            <WelcomePageOptionButtons
-              btnText="New Project"
-              onClick={() => {
-                setIsOpen(true);
-              }}
-            />
-            <NewProjectSizeAndColourSelectionModal
-              open={isOpen}
-              closeModal={setIsOpen}
-            />
-            <Link to="/">
-              <Logo />
-            </Link>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </>
-  );
+  return <NavBar />;
 };
---
+++ src/components/NavBar.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { AppBar } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+
+import wool from "../images/wool.jpg";
+import { WelcomePageOptionButtons } from "./WelcomePageOptionButtons";
+import { Logo } from "./Logo";
+import { NewProjectSizeAndColourSelectionModal } from "./NewProjectSizeAndColourSelectionModal";
+
+export const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <AppBar
+        postion="sticky"
+        sx={{ backgroundImage: `url(${wool})`, padding: "10px" }}
+      >
+        <Toolbar>
+          <Grid container direction="row" justifyContent="flex-end">
+            <WelcomePageOptionButtons
+              btnText="New Project"
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            />
+            <NewProjectSizeAndColourSelectionModal
+              open={isOpen}
+              closeModal={setIsOpen}
+            />
+            <Link to="/ViewProject">
+              <WelcomePageOptionButtons btnText={"View Projects"} />
+            </Link>
+            <Link to="/">
+              <Logo />
+            </Link>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
