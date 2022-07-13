e9d77ba631c61720f0fe854d28fe012f580c1902
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-07-13 12:59:56 -0230

    Update ColorPicker.js

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,15 +1,44 @@
-import {useState} from "react";
-import reactCSS from "reactcss";
+import { useState } from "react";
 import { GithubPicker } from "react-color";
 
-const ColorPicker = () => {
+const styles = {
+  default: {
+    color: {
+      width: "36px",
+      height: "14px",
+      borderRadius: "2px",
+      background: `rgba(${setColor.color.r}, ${setColor.color.g}, ${setColor.color.b}, ${setColor.color.a})`,
+    },
+    swatch: {
+      padding: "5px",
+      background: "#fff",
+      borderRadius: "5px",
+      boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+      display: "inline-block",
+      cursor: "pointer",
+    },
+    popover: {
+      position: "absolute",
+      zIndex: "2",
+    },
+    cover: {
+      position: "fixed",
+      top: "0px",
+      right: "0px",
+      bottom: "0px",
+      left: "0px",
+    },
+  },
+};
+
+export const ColorPicker = () => {
   const [isDisplayColorPicker, setIsDsiplayColorPicker] = useState(false);
   const [color, setColor] = useState({
-    r:"241",
-    g:"112",
-    b:"19",
-    a:"1",
-  })
+    r: "241",
+    g: "112",
+    b: "19",
+    a: "1",
+  });
 
   handleClick = () => {
     setIsDsiplayColorPicker(true);
@@ -20,57 +49,20 @@ const ColorPicker = () => {
   };
 
   handleChange = (color) => {
-    setColor(color.rgb)
+    setColor(color.rgb);
   };
 
-  render() {
-    const styles = reactCSS({
-      default: {
-        color: {
-          width: "36px",
-          height: "14px",
-          borderRadius: "2px",
-          background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
-        },
-        swatch: {
-          padding: "5px",
-          background: "#fff",
-          borderRadius: "5px",
-          boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
-          display: "inline-block",
-          cursor: "pointer",
-        },
-        popover: {
-          position: "absolute",
-          zIndex: "2",
-        },
-        cover: {
-          position: "fixed",
-          top: "0px",
-          right: "0px",
-          bottom: "0px",
-          left: "0px",
-        },
-      },
-    });
-
-    return (
-      <div>
-        <div style={styles.swatch} onClick={this.handleClick}>
-          <div style={styles.color} />
-        </div>
-        {this.state.displayColorPicker ? (
-          <div style={styles.popover}>
-            <div style={styles.cover} onClick={this.handleClose} />
-            <GithubPicker
-              color={this.state.color}
-              onChange={this.handleChange}
-            />
-          </div>
-        ) : null}
+  return (
+    <div>
+      <div style={styles.swatch} onClick={handleClick()}>
+        <div style={styles.color} />
       </div>
-    );
-  }
-}
-
-export default ColorPicker;
+      {this.state.displayColorPicker ? (
+        <div style={styles.popover}>
+          <div style={styles.cover} onClick={handleClose()} />
+          <GithubPicker color={color} onChange={handleChange()} />
+        </div>
+      ) : null}
+    </div>
+  );
+};
