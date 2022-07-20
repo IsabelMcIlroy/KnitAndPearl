062ef40edd9f9db889b5fb9b2e3464f28143be41
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-07-20 08:39:46 -0230

    pages...

--- src/index.js
+++ src/index.js
@@ -60,9 +60,15 @@ const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
-      <Route path="/NewProject" element={<NewProject />} />
-      <Route path="/ViewProject" element={<ViewProject />} />
-      <Route path="/" element={<Index />} />
+      <Route
+        path="/KnittingProjectManager/NewProject"
+        element={<NewProject />}
+      />
+      <Route
+        path="/KnittingProjectManager/ViewProject"
+        element={<ViewProject />}
+      />
+      <Route path="/KnittingProjectManager/" element={<Index />} />
     </Routes>
   </BrowserRouter>,
   rootElement
