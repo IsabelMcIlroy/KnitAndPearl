3cbed32393c60afcb20da5fa9b04cf8b8b39676d
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-07-26 09:50:07 -0230

    createRoot

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import "./index.css";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NewProject } from "./NewProject";
 import { ViewProject } from "./ViewProject";
 import { App } from "./App";
 
-const rootElement = document.getElementById("root");
-render(
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(
   <BrowserRouter>
     <Routes>
       <Route path="/NewProject" element={<NewProject />} />
       <Route path="/ViewProject" element={<ViewProject />} />
       <Route path="/KnittingProjectManager/" element={<App />} />
     </Routes>
-  </BrowserRouter>,
-  rootElement
+  </BrowserRouter>
 );
