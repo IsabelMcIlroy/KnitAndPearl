995cfb2e38fc04e4395a50d6cb0a3ae7cce552d2
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-07-26 09:33:01 -0230

    app.js

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./index.css";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import { Title } from "./components/title";
+import { WelcomePageOptionButtons } from "./components/WelcomePageOptionButtons";
+import wool from "./images/wool.jpg";
+import { Logo } from "./components/logo";
+import { NewProjectSizeAndColourSelectionModal } from "./components/NewProjectSizeAndColourSelectionModal";
+
+export const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url(${wool})`,
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Box
+        sx={{
+          textAlign: "center",
+          paddingTop: "100px",
+        }}
+      >
+        <Title titleText={"Knitting Project Manager"} />
+
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <WelcomePageOptionButtons
+            btnText={"New Project"}
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          />
+          <NewProjectSizeAndColourSelectionModal
+            open={isOpen}
+            setIsOpen={setIsOpen}
+          />
+
+          <Link to="/">
+            <Logo />
+          </Link>
+          <Link to="/ViewProject">
+            <WelcomePageOptionButtons btnText={"View Projects"} />
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,58 +1,9 @@
-import { useState } from "react";
 import "./index.css";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Box from "@mui/material/Box";
-import { Title } from "./components/title";
-import { WelcomePageOptionButtons } from "./components/WelcomePageOptionButtons";
-import wool from "./images/wool.jpg";
-import { Logo } from "./components/logo";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NewProject } from "./NewProject";
-import { NewProjectSizeAndColourSelectionModal } from "./components/NewProjectSizeAndColourSelectionModal";
 import { ViewProject } from "./ViewProject";
-
-export const Index = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <Box
-      sx={{
-        backgroundImage: `url(${wool})`,
-        height: "100vh",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
-      <Box
-        sx={{
-          textAlign: "center",
-          paddingTop: "100px",
-        }}
-      >
-        <Title titleText={"Knitting Project Manager"} />
-
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <WelcomePageOptionButtons
-            btnText={"New Project"}
-            onClick={() => {
-              setIsOpen(true);
-            }}
-          />
-          <NewProjectSizeAndColourSelectionModal
-            open={isOpen}
-            setIsOpen={setIsOpen}
-          />
-
-          <Link to="/">
-            <Logo />
-          </Link>
-          <Link to="/ViewProject">
-            <WelcomePageOptionButtons btnText={"View Projects"} />
-          </Link>
-        </Box>
-      </Box>
-    </Box>
-  );
-};
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 render(
@@ -60,7 +11,7 @@ render(
     <Routes>
       <Route path="/NewProject" element={<NewProject />} />
       <Route path="/ViewProject" element={<ViewProject />} />
-      <Route path="/KnittingProjectManager/" element={<Index />} />
+      <Route path="/KnittingProjectManager/" element={<App />} />
     </Routes>
   </BrowserRouter>,
   rootElement
