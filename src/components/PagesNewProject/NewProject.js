585ab43326d7e66c2c978c382c2e9cde517cc87d
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-08-02 13:11:54 -0230

    A grid...

--- src/components/PagesNewProject/NewProject.js
+++ src/components/PagesNewProject/NewProject.js
@@ -1,4 +1,6 @@
 import { useLocation } from "react-router-dom";
+import _ from "lodash";
+import { Grid, Box } from "@mui/material";
 import { NewProjectEditorDrawer } from "./HelperComponents/NewProjectEditorDrawer";
 
 export const NewProject = () => {
@@ -12,11 +14,30 @@ export const NewProject = () => {
   console.log(currentRows);
   console.log(currentColumns);
   return (
-    <>
+    <Box sx={{ display: "flex" }}>
       <NewProjectEditorDrawer
         currentProjectNameForDrawer={currentProjectName}
         currentProjectTypeForDrawer={currentProjectType || "---"}
       />
-    </>
+      {_.range(0, currentColumns).map((value) => (
+        <Grid
+          container
+          key={value}
+          value={value}
+          spacing={currentRows}
+          sx={{ marginTop: "150px", marginLeft: "100px" }}
+        >
+          {_.range(0, currentRows).map((value) => (
+            <Grid
+              item
+              key={value}
+              value={value}
+              xs={currentRows / 12}
+              sx={{ border: "1px solid black", height: "50px" }}
+            ></Grid>
+          ))}
+        </Grid>
+      ))}
+    </Box>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<App />);
+rootElement.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
