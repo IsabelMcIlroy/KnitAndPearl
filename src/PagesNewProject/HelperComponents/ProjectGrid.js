de662a95c7d4cbd1fdc8926af698fccf2f218a32
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-08-09 15:10:09 -0230

    grid troubles...

---
+++ src/PagesNewProject/HelperComponents/ProjectGrid.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import _ from "lodash";
+import { Grid, Button } from "@mui/material";
+
+export const ProjectGrid = (currentRows, currentColumns) => {
+  const [background, setBackground] = useState("#e9e1ec");
+  const setKnittingGridColor = (background) => {
+    setBackground(background);
+  };
+  return (
+    <>
+      <Grid
+        container
+        spacing={12 / { currentRows }}
+        sx={{
+          margin: "112px 80px 0px 80px",
+          justifyContent: "center",
+        }}
+      >
+        {_.range({ currentRows } * { currentColumns }, 0).map((value) => (
+          <Grid
+            item
+            key={value}
+            value={value}
+            xs={12 / { currentRows }}
+            sx={{
+              border: "1px solid black",
+              height: "50px",
+            }}
+          >
+            <Button
+              value={value}
+              sx={{
+                backgroundColor: `${background}`,
+                height: "100%",
+                minWidth: "90%",
+              }}
+              onClick={() => setKnittingGridColor("#000000")}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <Grid
+        container
+        spacing={1}
+        sx={{
+          marginTop: "112px",
+          marginLeft: "-4%",
+          justifyContent: "end",
+          position: "absolute",
+          zIndex: "-10",
+        }}
+      >
+        {_.range({ currentColumns }, 0).map((value) => (
+          <Grid
+            item
+            key={value}
+            value={value}
+            xs={12}
+            sx={{
+              height: "50px",
+              textAlign: "end",
+            }}
+          >
+            {value}
+          </Grid>
+        ))}
+      </Grid>
+      <Grid
+        container
+        spacing={12 / { currentRows }}
+        sx={{ margin: "0 80px 0 80px", justifyContent: "center" }}
+      >
+        {_.range({ currentRows }, 0).map((value) => (
+          <Grid
+            item
+            key={value}
+            value={value}
+            xs={12 / { currentRows }}
+            sx={{
+              height: "50px",
+              textAlign: "center",
+            }}
+          >
+            {value}
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
+};
--- src/PagesNewProject/NewProject.js
+++ src/PagesNewProject/NewProject.js
@@ -1,8 +1,7 @@
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
-import _ from "lodash";
-import { Grid, Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 import { NewProjectEditorDrawer } from "./HelperComponents/NewProjectEditorDrawer";
+import { ProjectGrid } from "./HelperComponents/ProjectGrid";
 
 export const NewProject = () => {
   const { state } = useLocation();
@@ -12,93 +11,14 @@ export const NewProject = () => {
     currentRows,
     currentColumns,
   } = state;
-  const [background, setBackground] = useState("#e9e1ec");
-  const setKnittingGridColor = (background) => {
-    setBackground(background);
-  };
+  console.log(currentColumns);
   return (
     <Box sx={{ display: "flex", flexWrap: "wrap" }}>
       <NewProjectEditorDrawer
         currentProjectNameForDrawer={currentProjectName}
         currentProjectTypeForDrawer={currentProjectType}
       />
-      <Grid
-        container
-        spacing={12 / currentRows}
-        sx={{
-          margin: "112px 80px 0px 80px",
-          justifyContent: "center",
-        }}
-      >
-        {_.range(currentRows * currentColumns, 0).map((value) => (
-          <Grid
-            item
-            key={value}
-            value={value}
-            xs={12 / currentRows}
-            sx={{
-              border: "1px solid black",
-              height: "50px",
-            }}
-          >
-            <Button
-              value={value}
-              sx={{
-                backgroundColor: `${background}`,
-                height: "100%",
-                minWidth: "90%",
-              }}
-              onClick={() => setKnittingGridColor("#000000")}
-            />
-          </Grid>
-        ))}
-      </Grid>
-      <Grid
-        container
-        spacing={1}
-        sx={{
-          marginTop: "112px",
-          marginLeft: "-4%",
-          justifyContent: "end",
-          position: "absolute",
-          zIndex: "-10",
-        }}
-      >
-        {_.range(currentColumns, 0).map((value) => (
-          <Grid
-            item
-            key={value}
-            value={value}
-            xs={12}
-            sx={{
-              height: "50px",
-              textAlign: "end",
-            }}
-          >
-            {value}
-          </Grid>
-        ))}
-      </Grid>
-      <Grid
-        container
-        spacing={12 / currentRows}
-        sx={{ margin: "0 80px 0 80px", justifyContent: "center" }}
-      >
-        {_.range(currentRows, 0).map((value) => (
-          <Grid
-            item
-            key={value}
-            value={value}
-            xs={12 / currentRows}
-            sx={{
-              height: "50px",
-              textAlign: "center",
-            }}
-          >
-            {value}
-          </Grid>
-        ))}
-      </Grid>
+      <ProjectGrid currentRows={currentRows} currentColumns={currentColumns} />
     </Box>
   );
 };
