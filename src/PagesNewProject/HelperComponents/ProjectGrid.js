9049c92cfe4a43c8780dfcd11c2b82355946da67
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-08-16 15:51:15 -0230

    start of grid rethink...

--- src/PagesNewProject/HelperComponents/ProjectGrid.js
+++ src/PagesNewProject/HelperComponents/ProjectGrid.js
@@ -1,24 +1,18 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import _ from "lodash";
 import { Grid, Button } from "@mui/material";
 
-export const ProjectGrid = (color) => {
+export const ProjectGrid = () => {
   const { state } = useLocation();
   const { currentRows, currentColumns } = state;
-  console.log(color);
-  const gridArray = Array(
-    ...Array(parseInt(currentRows * currentColumns)).keys()
-  );
-  const [background, setBackground] = useState("#e9e1ec");
-  const handleChangeColor = (index) => {
-    console.log(index);
-    for (let i = 0; i > gridArray.length; i++) {
-      if ((i = index)) {
-        gridArray[index].color = setBackground("#000000");
-      }
-    }
-  };
+  useEffect(() => {
+    let gridArray = Array(parseInt(currentRows))
+      .fill(0)
+      .map(() => new Array(parseInt(currentColumns)).fill(1));
+    console.log(gridArray);
+  });
+  const rowArray = Array(...Array(parseInt(currentRows)).keys());
+  const columnArray = Array(...Array(parseInt(currentRows)).keys());
   return (
     <>
       <Grid
@@ -27,84 +21,36 @@ export const ProjectGrid = (color) => {
         sx={{
           margin: "112px auto 0 auto",
           justifyContent: "center",
-          width: "80%",
+          width: "40%",
         }}
       >
-        {gridArray.map((index) => {
-          return (
-            <Grid
-              item
-              key={index}
-              value={index}
-              xs={12 / currentRows}
-              sx={{
-                border: "1px solid black",
-                aspectRatio: "1/1",
-              }}
-              style={{ padding: "4px" }}
-            >
-              <Button
-                value={index}
+        {rowArray.map((xindex) => {
+          return columnArray.map((yindex) => {
+            return (
+              <Grid
+                item
+                rowindex={xindex}
+                columnindex={yindex}
+                xs={12 / currentRows}
                 sx={{
-                  backgroundColor: `${background}`,
-                  height: "100%",
-                  minWidth: "100%",
+                  border: "1px solid black",
+                  aspectRatio: "1/1",
                 }}
-                key={index}
-                onClick={() => {
-                  handleChangeColor(index);
-                }}
-              />
-            </Grid>
-          );
+                style={{ padding: "4px" }}
+              >
+                <Button
+                  rowindex={xindex}
+                  columnindex={yindex}
+                  sx={{
+                    backgroundColor: "#E8E1EC",
+                    height: "100%",
+                    minWidth: "100%",
+                  }}
+                />
+              </Grid>
+            );
+          });
         })}
-      </Grid>
-      <Grid
-        container
-        spacing={1}
-        sx={{
-          marginTop: "112px",
-          marginLeft: "-4%",
-          justifyContent: "end",
-          position: "absolute",
-          zIndex: "-10",
-        }}
-      >
-        {_.range(currentColumns, 0).map((value) => (
-          <Grid
-            item
-            key={value}
-            value={value}
-            xs={12}
-            sx={{
-              maxWidth: "50px",
-              aspectRatio: "12 / 1",
-              textAlign: "end",
-            }}
-          >
-            {value}
-          </Grid>
-        ))}
-      </Grid>
-      <Grid
-        container
-        spacing={12 / currentRows}
-        sx={{ margin: "0 auto", justifyContent: "center", width: "80%" }}
-      >
-        {_.range(currentRows, 0).map((value) => (
-          <Grid
-            item
-            key={value}
-            value={value}
-            xs={12 / currentRows}
-            sx={{
-              height: "50px",
-              textAlign: "center",
-            }}
-          >
-            {value}
-          </Grid>
-        ))}
       </Grid>
     </>
   );
