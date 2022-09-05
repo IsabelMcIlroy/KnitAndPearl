a73e377214e2a779917d730fb0bef7c7a8d3306f
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-09-05 11:56:07 -0230

    column numbers back
    need to fix position of row numbers!!!!!

--- src/PagesNewProject/HelperComponents/ProjectGrid.js
+++ src/PagesNewProject/HelperComponents/ProjectGrid.js
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import _ from "lodash";
 import { Box, Grid, Typography } from "@mui/material";
 import { palette } from "../../assets/theme";
 import { ProjectGridCell } from "./ProjectGridCell";
@@ -12,7 +13,7 @@ export const ProjectGrid = ({
   currentProjectType,
 }) => {
   const { state } = useLocation();
-  const { currentColumns } = state;
+  const { currentColumns, currentRows } = state;
   const modifyGridColorArray = (xIndex, yIndex, currentlySelectedColor) => {
     gridArray[xIndex][yIndex] = currentlySelectedColor;
     setGridColors(gridArray);
@@ -60,6 +61,47 @@ export const ProjectGrid = ({
               );
             });
           })}
+        </Grid>
+        <Grid
+          container
+          spacing={1}
+          sx={{
+            marginTop: "112px",
+            marginLeft: "-4%",
+            justifyContent: "end",
+            position: "absolute",
+            zIndex: "-10",
+          }}
+        >
+          {_.range(currentRows, 0).map((value) => (
+            <Grid
+              item
+              key={value}
+              value={value}
+              xs={12}
+              sx={{
+                height: "50px",
+                textAlign: "end",
+              }}
+            >
+              {value}
+            </Grid>
+          ))}
+        </Grid>
+        <Grid
+          container
+          spacing={12 / currentRows}
+          sx={{
+            margin: "24px 0 0 0",
+            maxWidth: "80vh",
+            minWidth: "300px",
+          }}
+        >
+          {_.range(currentColumns, 0).map((value) => (
+            <Grid item key={value} value={value} xs={12 / currentColumns}>
+              {value}
+            </Grid>
+          ))}
         </Grid>
       </Box>
     </Box>
