1bed1698bf07ba1d7f439240aeae0f1fdd92b78c
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-09-23 08:26:28 -0230

    Update logo.js

--- src/HelperComponents/logo.js
+++ src/HelperComponents/logo.js
@@ -3,7 +3,13 @@ import { Box } from "@mui/material";
 
 export const Logo = () => {
   return (
-    <Box sx={{ display: "inline-block", padding: "12px" }}>
+    <Box
+      sx={{
+        display: "inline-block",
+        paddingLeft: "12px",
+        paddingRight: "12px",
+      }}
+    >
       <img src={logo} alt="ball of wool" />
     </Box>
   );
