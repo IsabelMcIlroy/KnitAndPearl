c41c3addebf1fa445941b4e149c68e7884c4295d
Mitchell Hynes <ecumene@users.noreply.github.com>
2022-10-08 16:52:32 -0230

    Initial commit for the user API

---
+++ myapp/server.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const usersFile = fs.readFileSync("users_db.json").toString();
+const users = usersFile ? JSON.parse(usersFile).users : [];
+
+app.post("/users", (req, res) => {
+  const { name, password } = req.body;
+
+  const id = users.length + 1;
+
+  users.push({ id, name, password });
+  fs.writeFileSync("users_db.json", JSON.stringify({ users: users }));
+
+  res.send({ id, name, password });
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { password: _, ...foundUser } = users.find(
+    (user) => user.id === parseInt(id, 10)
+  );
+
+  res.send(foundUser);
+});
+
+app.listen(3000);
