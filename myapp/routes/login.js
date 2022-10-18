bcae45504b8fe598b9c3205f357bbf5ddaa37c67
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-10-18 16:01:35 -0230

    Update login.js

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-router.post(
-  "/login",
-  express.urlencoded({ extended: false }),
-  function (req, res) {
-    req.session.regenerate(function (err) {
-      if (err) next(err);
+router.post("/", express.urlencoded({ extended: false }), function (req, res) {
+  req.session.regenerate(function (err) {
+    if (err) next(err);
 
-      req.session.user = req.body.user;
+    req.session.user = req.body.user;
 
-      req.session.save(function (err) {
-        if (err) return next(err);
-        res.redirect("/");
-      });
+    req.session.save(function (err) {
+      if (err) return next(err);
+      res.redirect("/");
     });
-  }
-);
+  });
+});
 
 module.exports = router;
