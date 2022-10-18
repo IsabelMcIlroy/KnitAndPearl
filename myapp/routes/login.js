f9afe9163a8d499b01568f6fd83796ff22951816
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-10-18 15:58:32 -0230

    login?

---
+++ myapp/routes/login.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+router.post(
+  "/login",
+  express.urlencoded({ extended: false }),
+  function (req, res) {
+    req.session.regenerate(function (err) {
+      if (err) next(err);
+
+      req.session.user = req.body.user;
+
+      req.session.save(function (err) {
+        if (err) return next(err);
+        res.redirect("/");
+      });
+    });
+  }
+);
--- myapp/server.js
+++ myapp/server.js
@@ -1,17 +1,44 @@
 const express = require("express");
 const { initSqlite } = require("./database");
-app.use(session({ secret: "keyboard cat", cookie: { maxAge: 60000 } }));
+const escapeHtml = require("escape-html");
 
 initSqlite();
+
+app.use(session({ secret: "keyboard cat", cookie: { maxAge: 60000 } }));
 
 const app = express();
 
 app.use(express.json());
 
+function isAuthenticated(req, res, next) {
+  if (req.session.user) next();
+  else next("route");
+}
+
+app.get("/", isAuthenticated, function (req, res) {
+  res.send(
+    "hello, " +
+      escapeHtml(req.session.user) +
+      "!" +
+      ' <a href="/logout">Logout</a>'
+  );
+});
+
+app.get("/", function (req, res) {
+  res.send(
+    '<form action="/login" method="post">' +
+      'Username: <input name="user"><br>' +
+      'Password: <input name="pass" type="password"><br>' +
+      '<input type="submit" text="Login"></form>'
+  );
+});
+
 const userRouter = require("./routes/users");
 const projectRouter = require("./routes/projects");
+const loginRouter = require("./routes/login");
 
 app.use("/users", userRouter);
 app.use("/projects", projectRouter);
+app.use("/login", loginRouter);
 
 app.listen(3000);
