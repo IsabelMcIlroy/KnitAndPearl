35b4130769b0afd907b2c62702c8d4b3d30a2193
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-10-20 15:26:07 -0230

    Update Home.js

--- src/PagesHome/Home.js
+++ src/PagesHome/Home.js
@@ -7,12 +7,9 @@ import { WelcomePageOptionButtons } from "./HelperComponents/WelcomePageOptionBu
 import wool from "../images/wool.jpg";
 import { Logo } from "../HelperComponents/logo";
 import { NewProjectSizeAndColourSelectionModal } from "../HelperComponents/NewProjectSizeAndColourSelectionModal";
-import { LoginModal } from "./HelperComponents/LoginModal";
 
 export const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
-  const [isLogin, setIsLogin] = useState(true);
   return (
     <Box
       sx={{
@@ -33,42 +30,24 @@ export const Home = () => {
         <Box
           sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}
         >
-          {isLogin === true && (
-            <WelcomePageOptionButtons
-              btnText={"New Project"}
-              onClick={() => {
-                setIsOpen(true);
-              }}
-            />
-          )}
+          <WelcomePageOptionButtons
+            btnText={"New Project"}
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          />
           <NewProjectSizeAndColourSelectionModal
             open={isOpen}
             setIsOpen={setIsOpen}
           />
 
-          {isLogin === false && (
-            <Logo
-              onClick={() => {
-                setIsLoginOpen(true);
-              }}
-            />
-          )}
-          <LoginModal
-            open={isLoginOpen}
-            setIsOpen={setIsLoginOpen}
-            isLogin={setIsLogin}
-          />
-
-          {isLogin === true && <Logo />}
-
-          {isLogin === true && (
-            <Link
-              to="/KnittingProjectManager/ViewProject"
-              style={{ textDecoration: "none" }}
-            >
-              <WelcomePageOptionButtons btnText={"View Projects"} />
-            </Link>
-          )}
+          <Logo />
+          <Link
+            to="/KnittingProjectManager/ViewProject"
+            style={{ textDecoration: "none" }}
+          >
+            <WelcomePageOptionButtons btnText={"View Projects"} />
+          </Link>
         </Box>
       </Box>
     </Box>
