df68927720d6ff6306952f875896c5130712ef2b
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-10-25 10:59:36 -0230

    ...

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
-// Import like this:
-// const {db} = require("../database");
+const { db } = require("../database");
 
 router.post("/", express.urlencoded({ extended: false }), function (req, res) {
   req.session.regenerate(function (err) {
     if (err) next(err);
 
     // https://github.com/TryGhost/node-sqlite3/wiki/API
-    // db.run("UPDATE tbl SET name = ? WHERE id = ?", "bar", 2);
-    // Query : db.run("SELECT * FROM user WHERE username = ?", req.body.username)
+    db.run("UPDATE tbl SET name = ? WHERE id = ?", "bar", 2);
 
     req.session.user = req.body.user;
 
