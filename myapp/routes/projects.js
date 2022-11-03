a2bc5729025e8ae19107352fae735179a87e683e
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-11-03 11:23:15 -0230

    start of projects...

--- myapp/routes/projects.js
+++ myapp/routes/projects.js
@@ -6,14 +6,25 @@ const projectsFile = fs.readFileSync("projects_db.json").toString();
 const projects = projectsFile ? JSON.parse(projectsFile).projects : [];
 
 router.post("/", (req, res) => {
-  const { owner_id, name, type, grid_colors, created_at } = req.body;
-
-  const id = projects.length + 1;
-
-  projects.push({ id, owner_id, name, type, grid_colors, created_at });
-  fs.writeFileSync("projects_db.json", JSON.stringify({ projects: projects }));
-
-  res.send({ id, owner_id, name, type, grid_colors, created_at });
+  try {
+    let newProject = `INSERT INTO projects(owner_id, name, type, grid_colors) VALUES (?,?,?,?)`;
+    db.run(
+      newUser,
+      [req.body.owner_id, req.body.name, req.body.type, req.body.grid_colors],
+      () => {
+        req.session.project = {
+          owner_id: req.body.owner_id,
+          name: req.body.name,
+          type: req.body.type,
+          grid_colors: req.body.grid_colors,
+        };
+        res.redirect("/");
+      }
+    );
+  } catch (e) {
+    console.log(e);
+    res.redirect("/");
+  }
 });
 
 router
