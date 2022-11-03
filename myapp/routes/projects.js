b06d885312116455060f4b78aef886b4c5f64886
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-11-03 11:38:25 -0230

    opps...

--- myapp/routes/projects.js
+++ myapp/routes/projects.js
@@ -5,27 +5,31 @@ const router = express.Router();
 const projectsFile = fs.readFileSync("projects_db.json").toString();
 const projects = projectsFile ? JSON.parse(projectsFile).projects : [];
 
-router.post("/", (req, res) => {
-  try {
-    let newProject = `INSERT INTO projects(owner_id, name, type, grid_colors) VALUES (?,?,?,?)`;
-    db.run(
-      newUser,
-      [req.body.owner_id, req.body.name, req.body.type, req.body.grid_colors],
-      () => {
-        req.session.project = {
-          owner_id: req.body.owner_id,
-          name: req.body.name,
-          type: req.body.type,
-          grid_colors: req.body.grid_colors,
-        };
-        res.redirect("/");
-      }
-    );
-  } catch (e) {
-    console.log(e);
-    res.redirect("/");
+router.post(
+  "/",
+  express.urlencoded({ extended: false }),
+  async function (req, res) {
+    try {
+      let newProject = `INSERT INTO projects(owner_id, name, type, grid_colors) VALUES (?,?,?,?)`;
+      db.run(
+        newProject,
+        [req.body.owner_id, req.body.name, req.body.type, req.body.grid_colors],
+        () => {
+          req.session.project = {
+            owner_id: req.body.owner_id,
+            name: req.body.name,
+            type: req.body.type,
+            grid_colors: req.body.grid_colors,
+          };
+          res.redirect("/");
+        }
+      );
+    } catch (e) {
+      console.log(e);
+      res.redirect("/");
+    }
   }
-});
+);
 
 router
   .route("/:id")
