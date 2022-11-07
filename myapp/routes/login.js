c4d33dabe04851600001191619ef1e48b4929b2d
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-11-07 15:36:52 -0330

    Update login.js

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -27,7 +27,7 @@ router.post("/", function (req, res) {
           bcrypt.compare(req.body.password, row.password, (err, same) => {
             if (err) throw err;
             if (same) {
-              req.session.user = { username: row.username };
+              req.session.user = { id: row_id, username: row.username };
               res.json({ message: "welcome!" });
             } else {
               res.send({ error: "incorrect password!" });
