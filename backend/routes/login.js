672f1eccb09045157aa23c57a401e9a35769ac05
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-12-01 13:42:52 -0330

    Update login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,23 +4,23 @@ const { db } = require("../database");
 const bcrypt = require("bcrypt");
 
 router.post("/", async function (req, res) {
-  console.log(req.body);
-  // https://github.com/TryGhost/node-sqlite3/wiki/API
-  // Query
+  try {
+    const user = await db
+      .prepare("SELECT * FROM users WHERE username = ?")
+      .get(req.body.username);
+    const passwordMatches = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
 
-  const user = await db
-    .prepare("SELECT * FROM users WHERE username = ?")
-    .get(req.body.username);
-  const passwordMatches = await bcrypt.compare(
-    req.body.password,
-    user.password
-  );
-
-  if (passwordMatches) {
-    req.session.user = { id: user.id, username: user.username };
-    res.json({ message: "welcome!" });
-  } else {
-    res.json({ error: "incorrect password/username" });
+    if (passwordMatches) {
+      req.session.user = { id: user.id, username: user.username };
+      res.json({ message: "welcome!" });
+    } else {
+      res.json({ error: "incorrect password/username" });
+    }
+  } catch {
+    res.send(401);
   }
 });
 
