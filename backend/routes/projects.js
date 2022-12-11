3f9509cdcb098be2d2264c1c3fb115dde050c3ec
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-12-11 11:08:32 -0330

    session fix - add default grid_colours

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -7,14 +7,22 @@ router.post("/", isAuthenticated, async function (req, res) {
   let currentUser = req.session.user;
   const newProject = await db
     .prepare(
-      `INSERT INTO projects(owner_id, name, type, rows, columns) VALUES (?,?,?,?,?)`
+      `INSERT INTO projects(owner_id, name, type, rows, columns, grid_colours) VALUES (?,?,?,?,?,?)`
     )
     .run(
       currentUser.id,
       req.body.projectName,
       req.body.projectType,
       req.body.Row,
-      req.body.Column
+      req.body.Column,
+      JSON.stringify(
+        Array(req.body.Row * req.body.Column).fill({
+          r: 212,
+          g: 196,
+          b: 251,
+          a: 1,
+        })
+      )
     );
   req.session.project = newProject;
   res.json({ message: newProject });
@@ -37,7 +45,7 @@ router.put("/:id", isAuthenticated, async function (req, res) {
   const currentProject = req.session.project.id;
   const updateProject = await db
     .prepare(`UPDATE projects SET grid_colours = ? WHERE id = ?`)
-    .run(req.body.grid, currentProject);
+    .run(JSON.stringify(req.body.grid), currentProject);
   res.json(updateProject);
 });
 
