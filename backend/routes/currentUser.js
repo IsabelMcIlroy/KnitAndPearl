9bba7aa89126dc7d84e016691f6dd9a8b244b203
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-12-12 09:21:00 -0330

    change if to isAuth

--- backend/routes/currentUser.js
+++ backend/routes/currentUser.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const isAuthenticated = require("../isAuthenticated");
 
-router.get("/", async function (req, res) {
+router.get("/", isAuthenticated, async function (req, res) {
   const currentlyLoggedinUser = req.session.user;
-  if (currentlyLoggedinUser === undefined) {
-    res.json(null);
-  } else {
-    res.json({ username: currentlyLoggedinUser.username });
-  }
+  res.json({ username: currentlyLoggedinUser.username });
 });
 
 module.exports = router;
