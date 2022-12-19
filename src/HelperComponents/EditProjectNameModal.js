95535cc673ef83cfbddf4a36bef40e00b225be85
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-12-19 14:55:43 -0330

    opps

---
+++ src/HelperComponents/EditProjectNameModal.js
@@ -0,0 +1,144 @@
+import React, { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Modal,
+  ThemeProvider,
+  Typography,
+  Box,
+  TextField,
+} from "@mui/material";
+import KnittingTheme, { palette, modalTitle } from "../assets/theme";
+import { ModalButton } from "./ModalButton";
+
+export const EditProjectNameModal = ({ open, onClick, setIsOpen }) => {
+  const form = useRef();
+  const validationSchema = yup.object().shape({
+    projectName: yup.string().required("This is required"),
+  });
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ resolver: yupResolver(validationSchema) });
+  const [state, setState] = useState({
+    projectID: "",
+    projectName: "",
+    projectType: "",
+  });
+  const handleInput = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const onSubmit = async (data) => {
+    console.log(data);
+    const response = await fetch("/projects/editNames", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const payload = await response.json();
+    console.log(payload);
+    setIsOpen(false);
+  };
+  return (
+    <ThemeProvider theme={KnittingTheme}>
+      <Modal open={open} onClick={onClick}>
+        <Box
+          style={{
+            overflowY: "auto",
+            overflowStyle: "scroll",
+            transform: "translate(25%, 15%)",
+            display: "inline-block",
+            height: "80%",
+            maxHeight: "500px",
+            width: "70%",
+          }}
+        >
+          <Typography variant="h4" sx={modalTitle}>
+            New Knitting Project
+          </Typography>
+          <Box
+            sx={{
+              padding: "24px 24px 0 24px",
+              backgroundColor: palette.knittingGray,
+              borderRadius: "0 0 24px 24px",
+            }}
+          >
+            <form
+              ref={form}
+              onSubmit={handleSubmit(onSubmit)}
+              style={{ textAlign: "center" }}
+            >
+              <Box padding="10px">
+                <TextField
+                  required
+                  fullWidth
+                  {...register("projectName")}
+                  error={errors.projectName ? true : false}
+                  label="Project Name"
+                  name="projectName"
+                  value={state.projectName}
+                  onChange={handleInput}
+                  sx={{ width: "90%" }}
+                />
+                <Typography
+                  variant="inherit"
+                  color={palette.knittingErrorColour}
+                >
+                  {errors.projectName?.message}
+                </Typography>
+              </Box>
+              <Box padding="10px">
+                <TextField
+                  fullWidth
+                  {...register("projectType")}
+                  label="Project Type"
+                  name="projectType"
+                  value={state.projectType}
+                  onChange={handleInput}
+                  sx={{ width: "90%" }}
+                />
+              </Box>
+              <Box
+                textAlign="center"
+                sx={{
+                  display: "inline-flex",
+                  alignContent: "center",
+                  flexWrap: "wrap",
+                  textAlign: "center",
+                }}
+              >
+                <Typography
+                  variant="inherit"
+                  color={palette.knittingErrorColour}
+                >
+                  {errors.Column?.message}
+                </Typography>
+              </Box>
+              <Box textAlign="center">
+                <ModalButton
+                  onClick={handleSubmit(onSubmit)}
+                  text="Submit"
+                  variant="submit"
+                />
+                <ModalButton
+                  text="Cancel"
+                  variant="Cancel"
+                  onClick={() => {
+                    setIsOpen(false);
+                  }}
+                />
+              </Box>
+            </form>
+          </Box>
+        </Box>
+      </Modal>
+    </ThemeProvider>
+  );
+};
