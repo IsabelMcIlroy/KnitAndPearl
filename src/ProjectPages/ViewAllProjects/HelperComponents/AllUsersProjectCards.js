3124cbef72c3b165608c52e934069bd974a0a3e2
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2023-01-06 14:28:53 -0330

    checkbox - switch from all users projects to users

---
+++ src/ProjectPages/ViewAllProjects/HelperComponents/AllUsersProjectCards.js
@@ -0,0 +1,81 @@
+import React from "react";
+import useFetch from "react-fetch-hook";
+import { Box, Typography } from "@mui/material";
+import { palette } from "../../../assets/theme";
+import { ViewProjectCard } from "./ViewProjectCard";
+
+export const AllUsersProjectCards = ({ searchQuery }) => {
+  const {
+    isLoading,
+    data: allProjects,
+    error,
+  } = useFetch("/projects/allUsers");
+  const projectsToShow = allProjects?.filter(
+    (project) =>
+      project.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      project.type.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        margin: "0px 24px",
+        flexWrap: "wrap",
+        justifyContent: "space-evenly",
+      }}
+    >
+      {isLoading && (
+        <Typography
+          variant="p"
+          sx={{
+            color: palette.knittingPurple,
+            alignItems: "center",
+            display: "flex",
+            height: "50vh",
+          }}
+        >
+          Loading...
+        </Typography>
+      )}
+      {error && (
+        <Typography
+          variant="p"
+          sx={{
+            color: palette.knittingErrorColour,
+            alignItems: "center",
+            display: "flex",
+            height: "50vh",
+          }}
+        >
+          error
+        </Typography>
+      )}
+      {allProjects < 1 && (
+        <Typography
+          variant="p"
+          sx={{
+            color: palette.knittingPurple,
+            alignItems: "center",
+            display: "flex",
+            height: "50vh",
+          }}
+        >
+          You don't have an projects yet!
+        </Typography>
+      )}
+      {projectsToShow?.map((displayProjectsArray) => {
+        return (
+          <ViewProjectCard
+            key={displayProjectsArray.id}
+            projectName={displayProjectsArray.name}
+            projectType={displayProjectsArray.type}
+            projectID={displayProjectsArray.id}
+            currentRows={displayProjectsArray.rows}
+            currentColumns={displayProjectsArray.columns}
+            gridColours={displayProjectsArray.grid_colours}
+          />
+        );
+      })}
+    </Box>
+  );
+};
--- src/ProjectPages/ViewAllProjects/ViewProjects.js
+++ src/ProjectPages/ViewAllProjects/ViewProjects.js
@@ -1,27 +1,27 @@
-import { useState } from "react";
-import useFetch from "react-fetch-hook";
-import { Box, Typography, TextField, Fab } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  Typography,
+  TextField,
+  Fab,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 import { palette } from "../../assets/theme";
-import { ViewProjectCard } from "./HelperComponents/ViewProjectCard";
 import { NewProjectSizeAndColourSelectionModal } from "../../HelperComponents/NewProjectSizeAndColourSelectionModal";
+import { UsersProjectCards } from "./HelperComponents/UsersProjectCards";
+import { AllUsersProjectCards } from "./HelperComponents/AllUsersProjectCards";
 
 export const ViewProjects = () => {
-  const { isLoading, data: allProjects, error } = useFetch("/projects");
-  const {
-    isLoading: usersProjectsLoading,
-    data: allUsersProjects,
-    error: usersProjectsError,
-  } = useFetch("/projects/allUsers");
-  console.log(usersProjectsLoading);
-  console.log(allUsersProjects);
-  console.log(usersProjectsError);
   const [isOpen, setIsOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
-  const projectsToShow = allProjects?.filter(
-    (project) =>
-      project.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      project.type.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+  console.log(checked);
   return (
     <Box
       sx={{
@@ -50,73 +50,34 @@ export const ViewProjects = () => {
           >
             View Projects
           </Typography>
-          <TextField
-            label="Search Projects"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            margin: "0px 24px",
-            flexWrap: "wrap",
-            justifyContent: "space-evenly",
-          }}
-        >
-          {isLoading && (
-            <Typography
-              variant="p"
-              sx={{
-                color: palette.knittingPurple,
-                alignItems: "center",
-                display: "flex",
-                height: "50vh",
-              }}
-            >
-              Loading...
-            </Typography>
-          )}
-          {error && (
-            <Typography
-              variant="p"
-              sx={{
-                color: palette.knittingErrorColour,
-                alignItems: "center",
-                display: "flex",
-                height: "50vh",
-              }}
-            >
-              error
-            </Typography>
-          )}
-          {allProjects < 1 && (
-            <Typography
-              variant="p"
-              sx={{
-                color: palette.knittingPurple,
-                alignItems: "center",
-                display: "flex",
-                height: "50vh",
-              }}
-            >
-              You don't have an projects yet!
-            </Typography>
-          )}
-          {projectsToShow?.map((displayProjectsArray) => {
-            return (
-              <ViewProjectCard
-                key={displayProjectsArray.id}
-                projectName={displayProjectsArray.name}
-                projectType={displayProjectsArray.type}
-                projectID={displayProjectsArray.id}
-                currentRows={displayProjectsArray.rows}
-                currentColumns={displayProjectsArray.columns}
-                gridColours={displayProjectsArray.grid_colours}
+          <Box>
+            <TextField
+              label="Search Projects"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    sx={{
+                      color: palette.knittingPurple,
+                      "&.Mui-checked": {
+                        color: palette.knittingPurple,
+                      },
+                    }}
+                    checked={checked}
+                    onChange={handleChange}
+                  />
+                }
+                label="My Projects Only"
+                sx={{ color: palette.knittingPurple }}
               />
-            );
-          })}
+            </FormGroup>
+          </Box>
         </Box>
+        {checked === true && <UsersProjectCards searchQuery={searchQuery} />}
+        {!checked && <AllUsersProjectCards searchQuery={searchQuery} />}
       </Box>
       <Fab
         onClick={() => {
