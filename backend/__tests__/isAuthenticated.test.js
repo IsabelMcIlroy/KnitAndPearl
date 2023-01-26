129e0c8c9da82707b8a15655d2394c6c805396b6
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2023-01-25 21:02:22 -0330

    jest test

---
+++ backend/__tests__/isAuthenticated.test.js
@@ -0,0 +1,21 @@
+const isAuthenticated = require("../isAuthenticated");
+
+test("should pass 401 to next() if no user", () => {
+  const req = {};
+  const res = null;
+  const next = jest.fn();
+
+  isAuthenticated(req, res, next);
+  expect(next).toHaveBeenCalledTimes(1);
+  expect(next).toHaveBeenLastCalledWith(401);
+});
+
+test("should call next() if there is a user", () => {
+  const req = { session: { user: "Isabel" } };
+  const res = null;
+  const next = jest.fn();
+
+  isAuthenticated(req, res, next);
+  expect(next).toHaveBeenCalledTimes(1);
+  expect(next).toHaveBeenCalledWith();
+});
