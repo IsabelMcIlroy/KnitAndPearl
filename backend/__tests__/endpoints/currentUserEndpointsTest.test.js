f2571337bbaf62abcca31bf384ca177d43ea64e4
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2023-02-06 11:32:52 -0330

    endpoint testing working :)

--- backend/__tests__/endpoints/currentUserEndpointsTest.test.js
+++ backend/__tests__/endpoints/currentUserEndpointsTest.test.js
@@ -1,22 +1,10 @@
-/**
- * @jest-environment node
- **/
-//const server = require("../../server");
-//let _server;
-//const fetch = require("fetch");
+// import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
-beforeAll(() => {
-  _server = server.app.listen(3001);
-});
-
-test.todo("checking current user", async () => {
-  //   const resp = await fetch("http://localhost:3001/currentUser", {
-  //     "Content-Type": "application/json",
-  //   });
-  //   const user = await resp.json();
-  //   expect(user).toBeTruthy();
-});
-
-afterAll(() => {
-  _server.unref();
+test("checking current user", async () => {
+  const resp = await fetch("http://localhost:3001/currentUser", {
+    "Content-Type": "application/json",
+  });
+  const user = await resp.json();
+  expect(user).toBeNull();
 });
