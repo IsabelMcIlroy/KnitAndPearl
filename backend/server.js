ef95a64b656fad8d72de07f233acdd4646c95d4d
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2023-02-20 13:23:52 -0330

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,4 @@ app.get("/projects/:id", isAuthenticated, viewProjectHandler);
 app.put("/projects/:id", isAuthenticated, saveProjectHandler);
 app.put("/projects/editNames/:id", isAuthenticated, changeProjectNameHandler);
 
-const port = process.env.DB_PORT || 3001;
-
-app.listen(port, () => console.log(`Listening on port ${Number(port)}`));
+app.listen(DB_PORT);
